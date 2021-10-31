8ca486f2d0cfa6f5adfebf8121a92afe9fbbbb5b
Siddhartha Saxena <ss.siddharthasaxena@gmail.com>
2021-10-31 06:04:26 +0530

    s

--- bin/server.js
+++ bin/server.js
@@ -3,32 +3,36 @@
 /**
  * @type {any}
  */
-const WebSocket = require('ws')
-const http = require('http')
-const wss = new WebSocket.Server({ noServer: true })
-const setupWSConnection = require('./utils.js').setupWSConnection
+const WebSocket = require("ws");
+const http = require("http");
+const wss = new WebSocket.Server({ noServer: true });
+const setupWSConnection = require("./utils.js").setupWSConnection;
 
-const host = process.env.HOST || 'localhost'
-const port = process.env.PORT || 1234
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT || 1234;
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('okay')
-})
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("okay");
+});
 
-wss.on('connection', setupWSConnection)
+wss.on("connection", setupWSConnection);
 
-server.on('upgrade', (request, socket, head) => {
+server.on("upgrade", (request, socket, head) => {
   // You may check auth of request here..
   /**
    * @param {any} ws
    */
-  const handleAuth = ws => {
-    wss.emit('connection', ws, request)
-  }
-  wss.handleUpgrade(request, socket, head, handleAuth)
-})
+  const handleAuth = (ws) => {
+    wss.emit("connection", ws, request);
+  };
+  wss.handleUpgrade(request, socket, head, handleAuth);
+});
 
-server.listen({ host, port })
+// server.listen({ host, port })
 
-console.log(`running at '${host}' on port ${port}`)
+server.listen(port, host, function () {
+  console.log("Listening on port %d", server_port);
+});
+
+console.log(`running at '${host}' on port ${port}`);
